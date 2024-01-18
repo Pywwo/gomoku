1dcbbbeaeae3ba02d804721469972e031e7e6b72
Antoine Carlier <32668729+Pywwo@users.noreply.github.com>
2024-01-18 12:38:40 +0100

    Update gomoku.py

--- gomoku.py
+++ gomoku.py
@@ -3,21 +3,18 @@
 import sys
 import tree_minmax
 
-Start = False
-ENNEMY = 2
+ENEMY = 2
 ME = 1
-board = 0
+board = []
 
 def start(size):
     global Start, board
     if size < 5 or size > 20:
-        print ("ERROR unsupported size", flush=True)
+        print("ERROR unsupported size", flush=True)
     else:
         Start = True
-        board = []
-        for i in range(size):
-            board.append([0] * size)
-        print ("OK", flush=True)
+        board = [[0] * size for _ in range(size)]
+        print("OK", flush=True)
 
 def answer():
     global board
@@ -25,18 +22,15 @@ def answer():
     tmp_tree = tree_minmax.Tree(1, 2, board)
     to_play = tmp_tree.get_minmax_score()
     board[to_play[1]][to_play[2]] = 1
-    print ("{},{}".format(to_play[2], to_play[1]), flush=True)
+    print("{},{}".format(to_play[2], to_play[1]), flush=True)
 
 def begin():
-    global board
     answer()
-    return
 
 def turn(x, y):
     global board
-    board[y][x] = ENNEMY
+    board[y][x] = ENEMY
     answer()
-    return
 
 def board_fct():
     global board
@@ -49,42 +43,35 @@ def board_fct():
     answer()
 
 def info(cmd):
-    return
+    pass
 
 def about():
-    print ("name=\"bigbrain\", version=\"1.0\", author=\"antoine\", country=\"France\"", flush=True)
+    print("name=\"bigbrain\", version=\"1.0\", author=\"antoine\", country=\"France\"", flush=True)
+
+command_functions = {
+    "END": sys.exit,
+    "START": start,
+    "ABOUT": about,
+    "BEGIN": begin,
+    "TURN": turn,
+    "BOARD": board_fct,
+    "INFO": info
+}
 
 def exec_cmd(cmd):
     global Start, board
-    splitedCmd = cmd.split(' ')
-    if splitedCmd[0] == "END":
-        sys.exit(0)
-    elif splitedCmd[0] == "START":
-        start(int(splitedCmd[1]))
-        return
-    elif splitedCmd[0] == "ABOUT":
-        about()
-        return
-    elif splitedCmd[0] == "BEGIN":
-        begin()
-        return
-    elif splitedCmd[0] == "TURN":
-        pos = splitedCmd[1].split(',')
-        turn(int(pos[0]), int(pos[1]))
-        return
-    elif splitedCmd[0] == "BOARD":
-        board_fct()
-        return
-    elif splitedCmd[0] == "INFO":
-        info(cmd)
-        return
+    splited_cmd = cmd.split(' ')
+    command = splited_cmd[0]
+
+    if command in command_functions:
+        command_functions[command](*splited_cmd[1:])
     else:
-        print ("ERROR - unknown command", flush=True)
+        print("ERROR - unknown command", flush=True)
 
 def main():
     while True:
         line = input()
         exec_cmd(line)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
