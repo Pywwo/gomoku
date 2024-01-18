46975db52ce22499f1cb0110883def3ea5cb91da
Antoine Carlier <32668729+Pywwo@users.noreply.github.com>
2024-01-18 12:40:39 +0100

    Update score.py

--- score.py
+++ score.py
@@ -1,225 +1,53 @@
 #!/usr/bin/env python3
 
-import copy
-import check_win
+SCORE_4_ROW = [0, 2000000, 5000000]  # Score as 4 in a row
+SCORE_3_ROW = [0, 30000, 800000]  # Score 3 in a row
+SCORE_2_ROW = [0, 1000, 9000]  # Score 2 in a row
 
-SCORE_4_ROW = [0, 2000000, 5000000] #score as 4 in a row, 0 if no open ends, 100000 if 1 and 2000000 if two
-SCORE_3_ROW = [0, 30000, 800000] # score 3 in a row
-SCORE_2_ROW = [0, 1000, 9000] # score 2 in a row
+def in_bounds(i, j, size):
+    return 0 <= i < size and 0 <= j < size
 
-def four_in_a_row_line(board, i, j, checking):
-    if j + 4 > len(board):
+def in_a_row_line(board, i, j, checking, length):
+    if j + length > len(board):
         return [False]
-    if board[i][j + 1] == checking and board[i][j + 2] == checking and board[i][j + 3] == checking:
-        #open ends
-        open_ends = 0
-        if j + 4 < len(board) and board[i][j + 4] == 0:
-            open_ends += 1
-        if j - 1 >= 0 and board[i][j - 1] == 0:
-            open_ends += 1
-        return [True, SCORE_4_ROW[open_ends]]
-    return [False]
+    for k in range(1, length):
+        if board[i][j + k] != checking:
+            return [False]
+    return [True, 0]
 
-def three_in_a_row_line(board, i, j, checking):
-    if j + 3 > len(board):
+def in_a_row_diag(board, i, j, checking, length, direction):
+    if j + length * direction[1] > len(board) or i + length * direction[0] > len(board):
         return [False]
-    if board[i][j + 1] == checking and board[i][j + 2] == checking:
-        #open ends
-        open_ends = 0
-        check_left = j
-        while check_left - 1 >= 0:
-            if board[i][check_left - 1] != 0 and board[i][check_left - 1] != checking:
-                break
-            check_left -= 1
-        check_right = j + 2
-        while check_right + 1 < len(board):
-            if board[i][check_right + 1] != 0 and board[i][check_right + 1] != checking:
-                break
-            check_right += 1
-        if check_right - check_left < 4:
-            return [True, 0]
-        if j - 1 >= 0 and board[i][j - 1] == 0:
-            open_ends += 1
-        if j + 3 < len(board) and board[i][j + 3] == 0:
-            open_ends += 1
-        return [True, SCORE_3_ROW[open_ends]]
-    return [False]
+    for k in range(1, length):
+        if board[i + k * direction[0]][j + k * direction[1]] != checking:
+            return [False]
+    return [True, 0]
 
-def two_in_a_row_line(board, i, j, checking):
-    if j + 2 > len(board):
-        return [False]
-    if board[i][j + 1] == checking:
-        #open ends
-        open_ends = 0
-        check_left = j
-        while check_left - 1 >= 0:
-            if board[i][check_left - 1] != 0 and board[i][check_left - 1] != checking:
-                break
-            check_left -= 1
-        check_right = j + 1
-        while check_right + 1 < len(board):
-            if board[i][check_right + 1] != 0 and board[i][check_right + 1] != checking:
-                break
-            check_right += 1
-        if check_right - check_left < 4:
-            return [True, 0]
-        if j - 1 >= 0 and board[i][j - 1] == 0:
-            open_ends += 1
-        if j + 2 < len(board) and board[i][j + 2] == 0:
-            open_ends += 1
-        return [True, SCORE_2_ROW[open_ends]]
-    return [False]
-
-def calc_score_line(board, checking):
+def calc_score_line(board, checking, scores):
+    size = len(board)
     score = 0
-    j = 0
-    for i in range(len(board)):
-        while j < len(board):
+    for i in range(size):
+        for j in range(size):
             if board[i][j] != checking:
-                j += 1
                 continue
-            #calc pawns on board to opti
-            #check win
-            arr = four_in_a_row_line(board, i, j, checking)
-            if arr[0] == True:
-                score += arr[1]
-                j += 4
-                continue
-            arr = three_in_a_row_line(board, i, j, checking)
-            if arr[0] == True:
-                score += arr[1]
-                j += 3
-                continue
-            if score < 10000:
-                arr = two_in_a_row_line(board, i, j, checking)
-                if arr[0] == True:
-                    score += arr[1]
-                    j += 2
-                    continue
-            j += 1
-        j = 0
-    return score
-
-def four_in_a_row_diag(board, i, j, checking):
-    if j + 4 > len(board):
-        return [False]
-    if i + 4 > len(board):
-        return [False]
-    if board[i + 1][j + 1] == checking and board[i + 2][j + 2] == checking and board[i + 3][j + 3] == checking:
-        #open ends
-        open_ends = 0
-        if j + 4 < len(board) and i + 4 < len(board) and board[i + 4][j + 4] == 0:
-            open_ends += 1
-        if j - 1 >= 0 and i - 1 >= 0 and board[i - 1][j - 1] == 0:
-            open_ends += 1
-        board[i][j] = 0
-        board[i + 1][j + 1] = 0
-        board[i + 2][j + 2] = 0
-        board[i + 3][j + 3] = 0
-        return [True, SCORE_4_ROW[open_ends]]
-    return [False]
-
-def two_in_a_row_diag(board, i, j, checking):
-    if j + 2 > len(board):
-        return [False]
-    if i + 2 > len(board):
-        return [False]
-    if board[i + 1][j + 1] == checking:
-        #open ends
-        open_ends = 0
-        check_left = j
-        check_second_left = i
-        while check_left - 1 >= 0:
-            if check_second_left - 1 < 0:
-                break
-            if board[check_second_left - 1][check_left - 1] != 0 and board[check_second_left - 1][check_left - 1] != checking:
-                break
-            check_second_left -= 1
-            check_left -= 1
-        check_right = j + 1
-        check_second_right = i + 1
-        while check_right + 1 < len(board):
-            if check_second_right + 1 >= len(board):
-                break
-            if board[check_second_right + 1][check_right + 1] != 0 and board[check_second_right + 1][check_right + 1] != checking:
-                break
-            check_right += 1
-            check_second_right += 1
-        if check_right - check_left < 4:
-            board[i][j] = 0
-            board[i + 1][j + 1] = 0
-            return [True, 0]
-        if j - 1 >= 0 and i - 1 >= 0 and board[i - 1][j - 1] == 0:
-            open_ends += 1
-        if j + 2 < len(board) and i + 2 < len(board) and board[i + 2][j + 2] == 0:
-            open_ends += 1
-        board[i][j] = 0
-        board[i + 1][j + 1] = 0
-        return [True, SCORE_2_ROW[open_ends]]
-    return [False]
-
-def three_in_a_row_diag(board, i, j, checking):
-    if j + 3 > len(board):
-        return [False]
-    if i + 3 > len(board):
-        return [False]
-    if board[i + 1][j + 1] == checking and board[i + 2][j + 2] == checking:
-        #open ends
-        open_ends = 0
-        check_left = j
-        check_second_left = i
-        while check_left - 1 >= 0:
-            if check_second_left - 1 < 0:
-                break
-            if board[check_second_left - 1][check_left - 1] != 0 and board[check_second_left - 1][check_left - 1] != checking:
-                break
-            check_second_left -= 1
-            check_left -= 1
-        check_right = j + 2
-        check_second_right = i + 2
-        while check_right + 1 < len(board):
-            if check_second_right + 1 >= len(board):
-                break
-            if board[check_second_right + 1][check_right + 1] != 0 and board[check_second_right + 1][check_right + 1] != checking:
-                break
-            check_right += 1
-            check_second_right += 1
-        if check_right - check_left < 4:
-            board[i][j] = 0
-            board[i + 1][j + 1] = 0
-            board[i + 2][j + 2] = 0
-            return [True, 0]
-        if j - 1 >= 0 and i - 1 >= 0 and board[i - 1][j - 1] == 0:
-            open_ends += 1
-        if j + 3 < len(board) and i + 3 < len(board) and board[i + 3][j + 3] == 0:
-            open_ends += 1
-        board[i][j] = 0
-        board[i + 1][j + 1] = 0
-        board[i + 2][j + 2] = 0
-        return [True, SCORE_3_ROW[open_ends]]
-    return [False]
-
-def calc_score_diag(board, checking):
-    new_board = copy.deepcopy(board)
-    score = 0
-    for i in range(len(new_board)):
-        for j in range(len(new_board)):
-            if new_board[i][j] != checking:
-                continue
-            arr = four_in_a_row_diag(new_board, i, j, checking)
-            if arr[0] == True:
-                score += arr[1]
-                continue
-            arr = three_in_a_row_diag(new_board, i, j, checking)
-            if arr[0] == True:
-                score += arr[1]
-                continue
-            if score < 10000:
-                arr = two_in_a_row_diag(new_board, i, j, checking)
-                if arr[0] == True:
-                    score += arr[1]
-                    continue
+            for length, score_value in enumerate(scores[1:], start=1):
+                if in_bounds(i, j + length, size):
+                    arr = in_a_row_line(board, i, j, checking, length + 1)
+                    if arr[0]:
+                        score += score_value
+            for length, score_value, direction in zip(range(1, 4), scores[1:], [(1, 0), (0, 1), (1, 1)]):
+                if in_bounds(i + length, j + length, size):
+                    arr = in_a_row_diag(board, i, j, checking, length + 1, direction)
+                    if arr[0]:
+                        score += score_value
     return score
 
 def calc_score(board, checking):
-    return calc_score_line(board, checking) + calc_score_line(list(zip(*reversed(board))), checking) + calc_score_diag(board, checking) + calc_score_diag([list(item) for item in list(zip(*reversed(board)))], checking)
+    return (calc_score_line(board, checking, SCORE_4_ROW) +
+            calc_score_line(list(zip(*reversed(board))), checking, SCORE_4_ROW) +
+            calc_score_line(board, checking, SCORE_3_ROW) +
+            calc_score_line(list(zip(*reversed(board))), checking, SCORE_3_ROW) +
+            calc_score_line(board, checking, SCORE_2_ROW) +
+            calc_score_line(list(zip(*reversed(board))), checking, SCORE_2_ROW) +
+            calc_score_line(list(zip(*reversed(board))), checking, SCORE_2_ROW) +
+            calc_score_line(board, checking, SCORE_2_ROW))
